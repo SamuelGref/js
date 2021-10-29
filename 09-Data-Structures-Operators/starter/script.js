53c7ab047cb24b1b1b0c0fbbe39c5c40d1801d71
Samuel Gref <samgref@gmail.com>
2021-10-29 10:06:17 -0400

    Array Destructuring

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -27,3 +27,71 @@ const restaurant = {
     },
   },
 };
+
+
+console.log("Strings pat 1 ....................................")
+
+
+const airline = 'Tap Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(airline.length);
+console.log('B320'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.lastIndexOf('Portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7)); // we can spaceify start and end of string
+
+// extracting unknown strings
+
+console.log('extracting unknown strings.......................')
+
+// console.log(airline.slice(0, airline.indexOf(' ')));
+// console.log(airline.slice(0, airline.lastIndexOf(' ')));
+console.log('Slice()...........................................')
+
+// console.log(airline.slice(-2));
+
+// The slice() method returns selected elements in an array, as a new array.
+
+// slice() selects the elements starting at the given start argument, and ends at, but does not include, the given end argument.
+
+// slice() does not change the original array.
+
+console.log(airline.slice(1, -1)); 
+
+const checkMiddleSeat = seat => {
+  // B and E middel seats
+  // to extract the last caracter we use the -1
+  const s = seat.slice(-1);
+  
+  if (s === 'B' || s === 'E') {
+    console.log('Middel Seat');
+  } else {
+    console.log('Not middel seat');
+  }  {
+
+  }
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('11C');
+checkMiddleSeat('11F');
+checkMiddleSeat('11E');
+
+
+
+// second element is not needed
+
+let arr = ['Samuel','Gref'] 
+
+let [firstName,lastName] = arr 
+
+let [a,b,c,d,e,f] = firstName
+
+console.log(typeof a,b,c,d,e,f)
+
